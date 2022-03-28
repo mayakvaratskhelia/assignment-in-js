8dda0b271bf0558ef794d2d54aa819918e0d0d5b
Maya kvaratskhelia <74621259+mayakvaratskhelia@users.noreply.github.com>
2022-03-28 04:45:09 +0400

    დასრულებული

--- დავალება #1/main.js
+++ დავალება #1/main.js
@@ -9,13 +9,14 @@
 
 // 1 კომენტარები
 
-/
-  // კომენტარები გაფუჭდა თქვენი დავალებაა
+
+  // კომენტარებმა გაფუჭდა თქვენი დავალებაა
   // არასწორად დაწერილი კომენტარი ჩაასწოროთ
   // და ასევე გამოიყენოთ თქვენი კოდის აღსაწერად
-*/
 
 
+  // კომენტარებისთვის ვიყენებთ // სლეშს თუ არვიცი რას ეძახით . :დ ...  ვფიქრობ ყველა
+  // გავასწორე.
 
 // 2 ცვლადები
 
@@ -31,78 +32,99 @@
 
 // >>>> კოდი დაწერეთ აქ
 
+let surname = 'Stjepan';
+let firsname = 'Hauser';
+let Age = 35;
+let birth = '15 June 1986';
+let date_of_birth =  1986;
+let place_of_birth= 'Yugoslavia';
+
+console.log(`სახელი: ${surname}`);
+console.log(`გვარი: ${firsname}`);
+console.log(`ასაკი: ${Age}`);
+console.log(`დაბადების წელი თვე და რიცხვი ${birth}`);
+console.log(`დაბადების წელი ${date_of_birth}`);
+console.log(`დაბადების ადგილი: ${place_of_birth}`);
+
+//ჩემებურად დავწერე რაღაცეები ჩვევასავით მაქ
+// და მოსახერხებელი იყო ჩემთვის.
 
 
 
-// 
 
-/
   // დროა დავიწყოთ მზადება ჩვენს მომავალ პროექტზე.
   // ჩვენი დავალეებაა მოვიფიქროთ რაიმე პროდუქტთა სია
   // რომელსაც გამოვიყენებთ მომავალში.
 
-  წიგნების მაღაზიის მაგალითი:
+  //წიგნების მაღაზიის მაგალითი:
 
-  const BookName = "ვეფხისტყაოსანი";
-  const AuthrName = "შოთა რუსთაველი";
-  const CreateDate = "13 საუკუნე";
-  let ProductQuant = 124;
-*/
+const BookName = "ვეფხისტყაოსანი";
+const AuthrName = "შოთა რუსთაველი";
+const CreateDate = "13 საუკუნე";
+let ProductQuant = 124;
+
+   // ჩემი მაგალითია   მოგზაურობა
+   const name= 'მოგზაურობა'
+const travel ='იტალია';
+const city= 'რომი';
+const food = 'პიცა';
+let rest = 15;
+
+  console.log(name);
+  console.log(travel);
+  console.log(city);
+  console.log(food);
+  console.log(rest);
 
 
-/ / >>>> კოდი დაწერეთ აქ
+//  >>>> კოდი დაწერეთ აქ
 
 
 //
 
-
-
 // 3 მონაცემთა ტიპები
 
-/*
   // აქ რაღაც შეცდომაა თქვენი დავალებაა ცვლადის გამოცხადების
   // დროს გაასწოროთ შეცდომა;
-*/
 
-
-
-const age = "1";
-const hisName = ["bob"];
-const isHigh = "true";
-const dateOfBirth = "1995";
-
-
+const age = 1;
+const hisName = 'bob';
+const isHigh = true;
+const dateOfBirth = 1995;
 
 
 // ეს კოდი ამოწმებს თქვენ მიერ მითითებულ ტიპებს
 
 // თქვენი მიზანია დააბრუნოთ პირველი 4 frue
 
-console.log(typeof age === "number");
-console.log(typeof hisName === "string");
-console.log(typeof isHigh === "boolean");
-console.log(typeof dateOfBirth === "number")
+console.log(typeof age != "number");
+console.log(typeof hisName != "string");
+console.log(typeof isHigh != "boolean");
+console.log(typeof dateOfBirth != "number")
 
 
 
-/ აქ უნდა გაიგოთ თქვენი პროდუქტის სიის ტიპი
+// აქ უნდა გაიგოთ თქვენი პროდუქტის სიის ტიპი
 
 // მაგ: console.log(typeof age)
-
+console.log(typeof(age), (age));
+console.log(typeof (hisName), (hisName) );
+console.log(typeof (isHigh), (isHigh));
+console.log(typeof (dateOfBirth), (dateOfBirth))
 
 
 // ოპერატორები არითმეტიკა
 
 let num = 5;
 
+
 // გამოიყენეთ ყველა ვარიანტი რომლითაც შეგვიძლია ამ ციფრის გაზრდა
 
 // >>> კოდი დაწერეთ აქ
-
-
-//
-
+num++;
+console.log(num + 5);
 console.log(num)
+
 
 
 
@@ -110,6 +132,20 @@ console.log(num)
 // მნიშვნელობები და გაიგოთ შეკრებილი მონაცემების ტიპი
 
 // >>> კოდი დაწერეთ აქ
+const x = 10;
+const y = 25;
+const z = 0;
+// არითმეტიკული ოპერატორები
+console.log(x + y);
+console.log(x - y);
+console.log(x / y);
+console.log(x * y);
+// ლოგიკური ოპერატორები
+console.log(z || y);
+console.log(z && y);
+// შედარებითი ოპერატორები
+console.log(z > y);
+console.log(z <= y);
+ 
 
-
-//
+// მგონი ყველა შევასრულე 
